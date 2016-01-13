c8785f217d014c3b7aee301f717b60b0ad4995fc
Evan Palmer <evjpalmer@gmail.com>
2016-01-13 15:20:35 -0800

    continue work on zombie dice
    move 'damage' from Player to Game method
    dedent ZombieDie methods
    add dice_on_table and player_brain count to Game
    Game.take_dice() and Game.do_player_turn() in progress

--- zombie-dice.py
+++ zombie-dice.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from random import randint, sample, shuffle
+from random import randint, shuffle
 
 
 class ZombieDie():
@@ -17,25 +17,24 @@ class ZombieDie():
         shuffle(self.sides)
         self.current_value = self.sides[0]
 
-        def roll(self):
-            self.current_value = self.sides[randint(0, len(self.sides) - 1)]
-            return self.current_value
+    def roll(self):
+        self.current_value = self.sides[randint(0, len(self.sides) - 1)]
+        return self.current_value
 
-        def __str__(self):
-            return '[' + str(self.current_value) + ']'
+    def __str__(self):
+        return '[' + str(self.current_value) + ']'
 
 
 class Player():
     def __init__(self, name):
         self.name = name
         self.brains = 0
-        self.damage = 0
 
     def add_brains(self, num):
         self.brains += num
 
-    def add_damage(self, num):
-        self.damage += num
+    def __str__(self):
+        return "{}, Brains={}".format(self.name, self.brains)
 
 
 class Game():
@@ -45,33 +44,55 @@ class Game():
         else:
             self.players = [Player('Player 1'), Player('Player 2')]
         self.current_player = self.players[0]
-        self.dice = self.create_dice()
-        self.current_round = 1
-        self.round_status = 'in progress'
+        self.dice_in_cup = self.create_dice()
+        self.dice_on_table = []
+        # for holding a player's accrued brains if the dice cup is refilled
+        self.player_brain_count = 0
 
     def create_dice(self):
-        dice = [ZombieDie('GREEN') for x in range(6)] + [ZombieDie('YELLOW') for x in range(4)] + [ZombieDie('RED') for x in range(3)]
+        dice = [ZombieDie('GREEN') for x in range(6)] \
+            + [ZombieDie('YELLOW') for x in range(4)] \
+            + [ZombieDie('RED') for x in range(3)]
         shuffle(dice)
         return dice
 
     def refill_dice_cup(self):
-        print("refilling dice cup, but not really")
+        pass
 
     def take_dice(self, num):
-        # TODO: handle empty dice cup
-        taken = self.dice[:num]
-        self.dice = self.dice[num:]
-        if len(self.dice) == 0:
-            self.refill_dice_cup()
+        taken = []
+        while len(self.dice_in_cup) > 0 and len(taken) < num:
+            taken.append(self.dice_in_cup.pop())
+        # there weren't enough dice
+        if len(taken) < num:
+            pass
 
     def do_player_turn(self):
         player = self.current_player
+        shotguns = footprints = 0
         print("{}, press any key to roll.")
         input()
-        current_dice = self.take_dice(3)
+        dice_on_table = self.take_dice(3)
+        values_showing = [die.current_value for die in dice_on_table]
         # display dice
         print("\nYou rolled: {}".format(current_dice))
         # check for three shotguns
+        if values_showing.count('S') == 3:
+            # end player's turn
+            print("{}, you got shotgunned!")
+            return
+        else:
+            shotguns += values_showing.count('S')
+            brains += values_showing.count('B')
+            footprints = values_showing.count('F')
+            if footprints == 0:
+                print("You ate {} brains, and took {} shotgun wounds this turn. Your turn is over.".format(self.player_brain_count, shotguns))
+                player.brains += self.player_brain_count
+            if footprints > 0:
+                # player may continue and re-roll footprints
+                print("You have {} brains, {} shotguns, and {} footprints this turn.".format(self.player_brain_count, shotguns, footprints))
+                if not confirm("Continue? (y/n): "):
+                    return
 
     def check_dice(self):
         pass
@@ -85,3 +106,15 @@ class Game():
         for player in self.players:
             player.brains = 0
         self.current_player = self.players[0]
+
+
+def confirm(prompt):
+    choice = input(prompt)
+    while True:
+        choice = choice.lower()
+        if choice == 'y' or choice.startswith('y'):
+            return True
+        elif choice == 'n' or choice.startswith('n'):
+            return False
+        else:
+            choice = input(prompt)
